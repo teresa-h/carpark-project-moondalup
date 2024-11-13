ced606737710523c3f98214813a99ffe68603cc7
J273389 <J273389@tafe.wa.edu.au>
2024-11-13 20:36:29 +0800

    feat: constructor and attributes to car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,12 @@
 class CarPark:
-    pass
+    def __init__(self, location="Unknown", capacity=200, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        return f"Car park at {self.location}, has {self.capacity} bays."
+
+    
